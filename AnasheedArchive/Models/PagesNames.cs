483bdcc1f429f480023fcd03dceadcd8a0077266
cabiste69 <55938130+cabiste69@users.noreply.github.com>
2024-05-05 14:45:08 +0100

    changed 'Categories' to 'Producers'

--- AnasheedArchive/Models/PagesNames.cs
+++ AnasheedArchive/Models/PagesNames.cs
@@ -5,7 +5,7 @@ public static class PagesNames
     public const string Home = "";
     public const string About = "about";
     public const string Anasheed = "anasheed";
-    public const string Categories = "categories";
+    public const string Producers = "Producers";
     public const string Tags = "tags";
     public const string News = "news";
 }
