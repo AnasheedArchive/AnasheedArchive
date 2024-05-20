0fd6cdfdf358f969bee4d53383a3d50cfbd365b0
cabiste69 <55938130+cabiste69@users.noreply.github.com>
2024-05-20 09:14:06 +0100

    parse lyrics as side by side columns

--- AnasheedArchive/ExtractTabs.cs
+++ AnasheedArchive/ExtractTabs.cs
@@ -47,12 +47,12 @@ class ExtractTabs
         }
 
         content.InsertRange(translationPos, ["", ""]);
-        AddBootstrapTabs(ref content, ref lyrics, translationPos);
+        AddBootstrapColumns(ref content, ref lyrics, translationPos);
 
         return content;
     }
 
-    private static void AddBootstrapTabs(ref List<string> content, ref Dictionary<string, List<string>> lyrics, int start)
+    private static void AddBootstrapTabs(ref List<string> content, ref Dictionary<string, List<string>> lyrics, int startPos)
     {
         string tabList = "<nav> <div class='nav nav-tabs' id='nav-tab' role='tablist'>";
         List<string> tabContent = new() { "<div class='tab-content' id='nav-tabContent'>" };
@@ -61,15 +61,106 @@ class ExtractTabs
         {
             tabList += $"<button class='nav-link' id='nav-{key}-tab' data-bs-toggle='tab' data-bs-target='#nav-{key}' type='button' role='tab' aria-controls='nav-{key}' aria-selected='true'>{key}</button>";
 
-            tabContent.Add($"<div class='tab-pane fade show' id='nav-{key}' role='tabpanel' aria-labelledby='nav-{key}-tab' tabindex='0'>");
+            tabContent.Add($"<div class='tab-pane fade show text-center' id='nav-{key}' role='tabpanel' aria-labelledby='nav-{key}-tab' tabindex='0'>");
             tabContent.AddRange(lyrics[key]);
             tabContent.Add("</div>");
         }
         tabList += "</div> </nav>";
         tabContent.Add("</div>");
 
-        content.InsertRange(start + 1, tabContent);
-        content.Insert(start, tabList);
+        content.InsertRange(startPos + 1, tabContent);
+        content.Insert(startPos, tabList);
 
+    }
+
+    private static void AddBootstrapTables(ref List<string> content, ref Dictionary<string, List<string>> lyrics, int startPos)
+    {
+        string table = "<div class='table-responsive'><table class='table text-center table-borderless align-middle'>";
+        string thead = $"<thead><tr>{GenerateTableHead(lyrics.Keys)}</tr></thead>";
+        string tbody = "<tbody>";
+
+        int i = 0;
+        while (i < lyrics.FirstOrDefault().Value.Count - 1)
+        {
+            tbody += "<tr>";
+            foreach (var lang in lyrics.Keys)
+            {
+                tbody += Tabalize(lyrics[lang][i]);
+            }
+            tbody += "</tr>";
+            i++;
+        }
+        // thead += "</tr></thead>";
+        tbody += "</tbody>";
+        table += thead + tbody + "</table></div>";
+        content.Insert(startPos, table);
+
+    }
+
+    private static string GenerateTableHead(Dictionary<string, List<string>>.KeyCollection keys)
+    {
+        var sum = "";
+        foreach (string key in keys)
+        {
+            sum += $"<th scope='col' class='pb-4'>{key}</th>";
+        }
+        return sum;
+    }
+
+    private static string Tabalize(string verse)
+    {
+        verse = verse.Trim();
+        if (verse.Length == 0)
+        {
+            return "";
+        }
+        if (verse.StartsWith('-'))
+        {
+            verse = verse.Remove(0, 1);
+        }
+        return $"<td class='pb-3'>{verse}</td>";
+    }
+
+    private static void AddBootstrapColumns(ref List<string> content, ref Dictionary<string, List<string>> lyrics, int startPos)
+    {
+        string container = "<div class='container'>";
+        string row = "<div class='text-center row row-cols-1 row-cols-sm-2'>";
+        string column = "<div class='col pb-5'>";
+
+        foreach (var translation in lyrics)
+        {
+            string nestedRow = $"<div class='row row-cols-1'>" + column + "<p class='h3'>" + translation.Key + "</p>" + "</div>";
+            foreach (var verse in translation.Value)
+            {
+                var verse2 = verse.Trim();
+                if (verse2.Length == 0)
+                {
+                    continue;
+                }
+                if (verse2.StartsWith('-'))
+                {
+                    verse2 = verse2.Remove(0, 1);
+                }
+                nestedRow += column + verse2 + "</div>";
+            }
+            row += nestedRow + "</div>" ;
+            if (translation.Key != lyrics.LastOrDefault().Key)
+            {
+                // todo: make the ruler hidden on mobile view
+                // row += "<div class='vr' style='padding: 0px; color: #ffffff'></div>";
+            }
+        }
+
+        content.Insert(startPos, container + row + "</div></div>");
+
+    }
+    private static string GenerateGridHead(Dictionary<string, List<string>>.KeyCollection keys)
+    {
+        var sum = "";
+        foreach (string key in keys)
+        {
+            sum += $"<th scope='col' class='pb-4'>{key}</th>";
+        }
+        return sum;
     }
 }
